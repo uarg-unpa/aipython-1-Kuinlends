51692572670b711e3c45a56d0c3aa2cf1cf875e9
Kuinlends <124259219+Kuinlends@users.noreply.github.com>
2024-03-01 20:09:40 +0000

    tarea

--- entrega/guia/principal.py
+++ entrega/guia/principal.py
@@ -1,4 +1,8 @@
 print ("Taller de AIpython PI E2")
 print ("Práctica","en","clase")
 print ("Hola",sep="", end="\n")
-print ("Hola","Gato", sep="°",end="*-*")
+print ("Hola","Gato", sep="°",end="\n")
+print (2*4 + (3*8))
+primer_nombre= 'Valeria'
+apellido_paterno='Centurión'
+print(primer_nombre, apellido_paterno)
