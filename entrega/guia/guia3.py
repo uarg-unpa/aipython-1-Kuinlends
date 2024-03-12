37eb79d0d365fa815fe6624ddae5344b9c37bebb
Kuinlends <124259219+Kuinlends@users.noreply.github.com>
2024-03-12 00:07:30 +0000

    change

---
+++ entrega/guia/guia3.py
@@ -0,0 +1,38 @@
+# Ejercicio 1
+num = 0
+while (num <= 100):
+    print(num)
+    num = num + 1
+
+# Ejercicio 2 
+for num in range(0,101):
+    print(num, end=" ")
+print(" ")
+# Ejercicio 3
+num = 10
+while (num >= 0):
+    print (num)
+    num = num - 1
+
+for num in range(10,-1,-1):
+    print(num, end=" ")
+print(" ")
+# Ejercicio 4
+num1 = int(input("Ingrese un número porfavor..."))
+num2 = int(input("Ingrese otro número porfavor..."))
+while(num1 == num2):
+    print("Debe ingresar dos números diferentes.")
+    num1 = int(input("Ingrese un número porfavor..."))
+    num2 = int(input("Ingrese otro número porfavor..."))
+# Fase comparativa (buscar el mayor)
+if (num1 > num2):
+    mayor = num1
+    menor = num2
+else:
+    mayor = num2
+    menor = num1
+
+for i in range(menor + 1, mayor):
+    print(i,end=", ")
+# Ejercicio 5
+    
