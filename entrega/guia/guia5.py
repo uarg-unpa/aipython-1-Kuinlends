41863b0558ee22244c10a252d7b9e5ae73e17cf4
Kuinlends <124259219+Kuinlends@users.noreply.github.com>
2024-03-17 21:29:19 +0000

    avance

--- entrega/guia/guia5.py
+++ entrega/guia/guia5.py
@@ -21,6 +21,31 @@ print(frutas[0], frutas[tope-1])
 # Ejercicio 6
 datos_personales= ['Valeria',21,1.55,'Soltero','Calle']
 # Ejercicio 7
+# a)
 companias=['Youtube','Spotify','Instagram']
+for nombre in companias:
+    print(nombre)
+# b)
+for i in range(len(companias)):
+    print(F" Indice: {i} , nombre de la compañia: {companias[i]}")
+# c)
+companias[2] = 'Discord'
+for nombre in companias:
+    print(nombre)
+
+# Ejercicio 8
+numeros = []
+for i in range(1,10):
+    numeros.append(i)
+print(numeros)
+sub_numeros = numeros[:3]
+print(sub_numeros)
+
+# Ejercicio 9
+letras = ['a','b','c','d','e','f','g','h','i','j']
+sub_letra = letras[::2]
+print(sub_letra)
+
+# Ejercicio 10
 
 
