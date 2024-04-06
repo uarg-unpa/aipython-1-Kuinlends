5b0a7e07091bc54845dc51092861f316997e92d0
Kuinlends <124259219+Kuinlends@users.noreply.github.com>
2024-04-06 03:31:51 +0000

    tpfinalcambios

--- entrega/guia/mainTareaFinalCenturion.py
+++ entrega/guia/mainTareaFinalCenturion.py
@@ -97,3 +97,109 @@ print("Opcion 3: Ver estadísticas ")
 print("Oprima otra tecla para salir. ")
 op = int(input("Ingrese un número del 1 al 3: "))
 match op:
+    case 1:
+        print("Usted ha seleccionado la opción 1: Tirada Simple, debe seleccionar el tipo de dado a lanzar")
+        print("Opcion 1: Tetraedro regular, 4 caras.")
+        print("Opcion 2: Cubo, 6 caras")
+        print("Opcion 3: Octaedro regular, 8 caras")
+        print("Opcion 4: Dodecaedro regular, 12 caras")
+        print("Opcion 5: Icosaedro regular, 20 caras")
+        print("Opcion 6: Trapezoedro pentagonal, 10 caras.")
+        op_dado =  int(input("Ingrese el número de la opción de dados a lanzar "))
+        cara = 0
+        match op_dado:
+            case 1:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 4))
+            case 2:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 6))
+            case 3:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 8))
+            case 4:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 12))
+            case 5:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 20))
+            case 6:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 10))
+            case _:
+                print("Opción inválida")
+    case 2:
+        print ("Usted ha seleccionado la opcion 2: Tirada Múltiple")
+        print("Opcion 1: Tetraedro regular, 4 caras.")
+        print("Opcion 2: Cubo, 6 caras")
+        print("Opcion 3: Octaedro regular, 8 caras")
+        print("Opcion 4: Dodecaedro regular, 12 caras")
+        print("Opcion 5: Icosaedro regular, 20 caras")
+        print("Opcion 6: Trapezoedro pentagonal, 10 caras.")
+        op_dado =  int(input("Ingrese el número de la opción de dados a lanzar "))
+        cara = 0
+        match op_dado:
+            case 1:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 4))
+            case 2:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 6))
+            case 3:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 8))
+            case 4:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 12))
+            case 5:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 20))
+            case 6:
+                cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                print(tiradaSimple(cant_lanzar, cara = 10))
+            case _:
+                print("Opción inválida")
+        res = False
+        while (res == False):
+            respuesta = str(input("¿Desea seguir lanzando dados? Responder 'Si/No'"))
+            respuesta.lower()
+            if respuesta == "no":
+                res = True 
+            else:
+                print("Opcion 1: Tetraedro regular, 4 caras.")
+                print("Opcion 2: Cubo, 6 caras")
+                print("Opcion 3: Octaedro regular, 8 caras")
+                print("Opcion 4: Dodecaedro regular, 12 caras")
+                print("Opcion 5: Icosaedro regular, 20 caras")
+                print("Opcion 6: Trapezoedro pentagonal, 10 caras.")
+                op_dado =  int(input("Ingrese el número de la opción de dados a lanzar: "))
+                cara = 0
+                match op_dado:
+                    case 1:
+                        cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                        print(tiradaSimple(cant_lanzar, cara = 4))
+                    case 2:
+                        cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                        print(tiradaSimple(cant_lanzar, cara = 6))
+                    case 3:
+                        cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                        print(tiradaSimple(cant_lanzar, cara = 8))
+                    case 4:
+                        cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                        print(tiradaSimple(cant_lanzar, cara = 12))
+                    case 5:
+                        cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                        print(tiradaSimple(cant_lanzar, cara = 20))
+                    case 6:
+                        cant_lanzar = int(input("Ingrese cantidad de dados a lanzar: "))
+                        print(tiradaSimple(cant_lanzar, cara = 10))
+                    case _:
+                        print("Opción inválida")
+    case 3:
+        print("Usted ha seleccionado ver estadisticas")
+        print(estadisticas(lanzamiento, lanzamientos_prom))
+    case _:
+        print("Opción no válida")
+        dados.clear()
+        resultados.clear()"""
+
